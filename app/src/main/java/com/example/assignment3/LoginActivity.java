ee16a3bd1177bf8e263800de44a0f54d27fcf6a9
AdarshStalu <200564265@student.georgianc.on.ca>
2025-04-11 13:39:53 -0400

    Adding login feature to our application

--- app/src/main/java/com/example/assignment3/LoginActivity.java
+++ app/src/main/java/com/example/assignment3/LoginActivity.java
@@ -4,37 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.assignment3.databinding.ActivityRegisterBinding;
+import com.example.assignment3.databinding.ActivityLoginBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    private ActivityRegisterBinding binding;
+    private ActivityLoginBinding binding;
     private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         auth = FirebaseAuth.getInstance();
 
-        binding.registerBtn.setOnClickListener(v -> {
+        binding.loginBtn.setOnClickListener(v -> {
             String email = binding.email.getText().toString().trim();
             String password = binding.password.getText().toString().trim();
 
-            auth.createUserWithEmailAndPassword(email, password)
+            auth.signInWithEmailAndPassword(email, password)
                     .addOnSuccessListener(authResult -> {
                         startActivity(new Intent(this, MainActivity.class));
                         finish();
                     }).addOnFailureListener(e ->
-                            Toast.makeText(this, "Failed: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+                            Toast.makeText(this, "Login failed: " + e.getMessage(), Toast.LENGTH_SHORT).show());
         });
 
-        binding.toLogin.setOnClickListener(v -> {
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-        });
+        binding.toRegister.setOnClickListener(v -> startActivity(new Intent(this, RegisterActivity.class)));
     }
 }
